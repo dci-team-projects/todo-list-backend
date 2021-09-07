f1fdbe0422c0f72c9b441ea7dbaeb8f3a04c0559
dci <priya.shah174@gmail.com>
2021-09-07 17:41:11 +0200

    registration givine back data

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,28 +2,6 @@ import { useState } from "react";
 // import formInputs from "../helpers/FormInputs";
 
 const Register = () => {
-  const formInputs = (
-    type = "",
-    inputName = "",
-    placeholder = "",
-    heading = ""
-  ) => {
-    return (
-      <div className="form-group pb-4">
-        <label htmlFor={inputName}>
-          <h5>{heading}</h5>
-        </label>
-        <input
-          type={type}
-          className="form-control"
-          id={inputName}
-          placeholder={placeholder}
-          onChange={handleChange}
-        />
-      </div>
-    );
-  };
-
   const data = {
     username: "",
     firstName: "",
@@ -65,17 +43,72 @@ const Register = () => {
   return (
     <div className="w-100 h-75 d-flex justify-content-center flex-column align-items-center">
       <h2 className="pb-5">Register here!</h2>
-      <form action="" className="w-25">
-        {formInputs(
-          "text",
-          "firstName",
-          "First Name",
-          "What is your first name?"
-        )}
-        {formInputs("text", "lastName", "Last Name", "What is your last name?")}
-        {formInputs("email", "email", "Email", "What is your email?")}
-        {formInputs("password", "password", "Password", "Password:")}
-        {formInputs("text", "username", "User Name", "User Name:")}
+      <form className="w-25">
+        <div className="form-group pb-4">
+          <label htmlFor="firstName">
+            <h5>First Name:</h5>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="firstName"
+            placeholder="First Name"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group pb-4">
+          <label htmlFor="lastName">
+            <h5>Last Name:</h5>
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group pb-4">
+          <label htmlFor="email">
+            <h5>Email</h5>
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group pb-4">
+          <label htmlFor="username">
+            <h5>User Name</h5>
+          </label>
+          <input
+            type="username"
+            className="form-control"
+            name="username"
+            placeholder="User Name"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group pb-4">
+          <label htmlFor="password">
+            <h5>Password</h5>
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </div>
+
         <input type="submit" value="submit" onClick={handleSubmit} />
       </form>
     </div>
