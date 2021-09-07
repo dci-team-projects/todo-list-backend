dfea595ffc6b2f64d9f7640455d0b51306d723d6
dci <priya.shah174@gmail.com>
2021-09-07 16:06:19 +0200

    finished creating the UI for the blog

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const formInputs = (
+    type = "",
+    inputName = "",
+    placeholder = "",
+    heading = ""
+  ) => {
+    return (
+      <div className="form-group pb-4">
+        <label htmlFor={inputName}>
+          <h5>{heading}</h5>
+        </label>
+        <input
+          type={type}
+          className="form-control"
+          id={inputName}
+          placeholder={placeholder}
+        ></input>
+      </div>
+    );
+  };
+  return (
+    <div className="w-100 h-75 d-flex justify-content-center flex-column align-items-center">
+      <h2 className="pb-5">Register here!</h2>
+      <form action="" className="w-25">
+        {formInputs(
+          "text",
+          "firstName",
+          "First Name",
+          "What is your first name?"
+        )}
+        {formInputs("text", "lastName", "Last Name", "What is your last name?")}
+        {formInputs("email", "email", "Email", "What is your email?")}
+        {formInputs("password", "password", "Password", "Password:")}
+      </form>
+    </div>
+  );
 };
 
 export default Register;
